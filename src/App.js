7e4a6e76dbd4b1030473a47e14f039eec54c0e2b
TinyDragon715 <vladimir715.han@gmail.com>
2022-08-18 13:38:57 +0200

    fix metamask mobile issue

--- src/App.js
+++ src/App.js
@@ -12,10 +12,38 @@ const smartContractAddress = '0x9d9fA9DbAe391C3FB6866F43De62FF3B393133b2';
 const xClubWalletAddress = '0x007a86bb3a8649590e84013dc62900632a8ec89f';
 const binancePegETHTokenAddress = '0x2170ed0880ac9a755fd29b2688956bd959f933f8';
 
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider,
+    display: {
+      name: "WalletConnect",
+      description: ""
+    },
+    options: {
+      rpc: {
+        56: 'https://bsc-dataseed.binance.org/'
+      },
+      network:'binance'
+    }
+  },
+};
+
+const web3Modal = new Web3Modal({
+  cacheProvider: true, // optional
+  providerOptions, // required
+  // disableInjectedProvider: false, // optional, For MetaMask / Brave / Opera.
+  theme: {
+    background: "rgb(39, 49, 56)",
+    main: "rgb(199, 199, 199)",
+    secondary: "rgb(136, 136, 136)",
+    border: "rgba(195, 195, 195, 0.14)",
+    hover: "rgb(16, 26, 32)"
+  }
+});
+
 function App() {
   let [provider, setProvider] = useState(null);
   const [walletConnected, setWalletConnected] = useState(false);
-  const [web3Modal, setWeb3Modal] = useState(null);
   const [totalRewardsDistributed, setTotalRewardsDistributed] = useState(null);
   const [xClubWalletDividendsInfo, setXClubWalletDividendsInfo] = useState(null);
   const [binancePegETHPrice, setBinancePegETHPrice] = useState(0);
@@ -33,50 +61,12 @@ function App() {
       .get(`https://api.coingecko.com/api/v3/simple/token_price/binance-smart-chain?contract_addresses=${binancePegETHTokenAddress}&vs_currencies=usd`)
       .then(res => res.data[binancePegETHTokenAddress].usd)
     );
-    
-    const providerOptions = {
-      walletconnect: {
-        package: WalletConnectProvider,
-        display: {
-          name: "WalletConnect",
-          description: ""
-        },
-        options: {
-          rpc: {
-            56: 'https://bsc-dataseed.binance.org/'
-          },
-          network:'binance'
-        }
-      },
-    };
-
-    let web3_Modal = new Web3Modal({
-      cacheProvider: false, // optional
-      providerOptions, // required
-      disableInjectedProvider: false, // optional, For MetaMask / Brave / Opera.
-      theme: {
-        background: "rgb(39, 49, 56)",
-        main: "rgb(199, 199, 199)",
-        secondary: "rgb(136, 136, 136)",
-        border: "rgba(195, 195, 195, 0.14)",
-        hover: "rgb(16, 26, 32)"
-      }
-    });
-
-    setWeb3Modal(web3_Modal);
   }
 
-  async function connectWallet() {
-    if(typeof window.ethereum === 'undefined') {
-      console.log("Please install the MetaMask");
-    } else {
-      if(!walletConnected) {
-        try {
-          provider = await web3Modal.connect();
-        } catch(e) {
-          console.log("Could not get a wallet connection", e);
-          return;
-        }
+  const connectWallet = async () => {
+    if(!walletConnected) {
+      try {
+        provider = await web3Modal.connect();
         setProvider(provider);
         // Subscribe to accounts change
         provider.on("accountsChanged", (accounts) => {
@@ -94,10 +84,13 @@ function App() {
         });
         setWalletConnected(true);
         await refreshAccountData();
-      } else {
-        setWalletConnected(!walletConnected);
-        await Disconnect();
+      } catch(e) {
+        console.log("Could not get a wallet connection", e);
+        return;
       }
+    } else {
+      setWalletConnected(!walletConnected);
+      await Disconnect();
     }
   }
 
@@ -116,7 +109,7 @@ function App() {
 
           const claimRes = await ProXContract.methods.claim().send({from: accounts[0]});
           if (claimRes) {
-           refreshAccountData(); 
+            await refreshAccountData(); 
           }
         } else {
           console.log("Please select the Binance Smart Chain Network");
